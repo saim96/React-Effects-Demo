ebdf5d2bfd0b4a015f9bde5f4b8a21068472a0cb
Saim Shujaat Khan <saimshujaatkhan@gmail.com>
2024-03-15 15:17:43 +0500

    recovery

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,13 @@
 import "./App.css";
-// import QuoteFetcher from "./QuoteFetcher";
 import Counter from "./counter";
-// import QuoteFetcherLoader from "./QuoteFetcherLoader";
-// import ProfileViewerWithSearch from "./ProfileViewerWithSearch";
+
 
 function App() {
   return (
     <>
       <h1>Counter</h1>
       <Counter />
+      <hr />
       
     </>
   );
