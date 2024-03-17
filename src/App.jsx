e8dea68ac48c477ad6aa7d5fd7f77497ff7b0679
Saim Shujaat Khan <saimshujaatkhan@gmail.com>
2024-03-18 03:10:30 +0500

    RenderDemo for useEffect added

--- src/App.jsx
+++ src/App.jsx
@@ -4,13 +4,16 @@ import QuoteFetcher from "./QouteFetcher.component";
 import Counter from "./counter";
 import QouteFetcherLoader from "./QuoteFetcherLoader.component";
 import ProfileViewerWithSearch from "./ProfileViewerWithSearch.component";
+import RenderDemo from "./RenderDemo"
 
 function App() {
   return (
     <>
       <ProfileViewerWithSearch />
       <hr />
-      <h1 >Counter</h1>
+      <RenderDemo/>
+      <hr />
+      <h1> Counter </h1>
       <Counter />
       <hr />
       <QuoteFetcher />
