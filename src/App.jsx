03a52759db0da6e5e6f49f96c4aa8f880129ecd4
Saim Shujaat Khan <saimshujaatkhan@gmail.com>
2024-03-19 23:58:59 +0500

    removed randerDemo

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,17 @@
 import "./index.css";
 import "./App.css";
 import QuoteFetcher from "./QouteFetcher.component";
-import Counter from "./counter";
+// import Counter from "./counter";
 import QouteFetcherLoader from "./QuoteFetcherLoader.component";
 import ProfileViewerWithSearch from "./ProfileViewerWithSearch.component";
-import RenderDemo from "./RenderDemo"
+// import RenderDemo from "./RenderDemo"
 
 function App() {
   return (
     <>
       <ProfileViewerWithSearch />
       <hr />
-      <RenderDemo/>
-      <hr />
-      <h1> Counter </h1>
-      <Counter />
-      <hr />
+      
       <QuoteFetcher />
       <hr />
       <QouteFetcherLoader />
